75fe0d14066775aea6ba215134d640250d71b14e
Sudhakar Puvvula <sudhakar.puvvula@ncr.com>
2023-08-22 20:06:46 +0300

    closure is used for

--- arguments.js
+++ arguments.js
@@ -1,10 +1,27 @@
-function sum(x) {
-    if (arguments.length == 2) {
-        return arguments[0] + arguments[1];
-    } else {
-        return function (y) { return x + y; };
-    }
+// function sum(x) {
+//     if (arguments.length == 2) {
+//         return arguments[0] + arguments[1];
+//     } else {
+//         return function (y) { return x + y; };
+//     }
+// }
+
+// console.log(sum(2, 3));   // Outputs 5
+// console.log(sum(2)(3));  // Outputs 5
+//=======================
+
+function sum(currentNumber) {
+    var total = currentNumber;
+    var func = function (nextNumber) {
+        if (nextNumber) {
+            total += nextNumber;
+            return func;
+        }
+        else {
+            return total;
+        }
+    };
+    return func;
 }
 
-console.log(sum(2, 3));   // Outputs 5
-console.log(sum(2)(3));  // Outputs 5
+console.log(sum(2)(5)(2)(5)());
