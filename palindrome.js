d1c74a421f87e92f2e63fb70f3200bcffa316c34
Sudhakar Puvvula <sudhakar.puvvula@ncr.com>
2023-08-31 08:55:21 +0300

    updated palindrome

--- palindrome.js
+++ palindrome.js
@@ -1,8 +1,9 @@
 function isPalindrome(str) {
-    str = str.replace(/\W/g, '').toLowerCase();
-    return (str == str.split('').reverse().join(''));
+    str = str.replace(/\W/g, '').toLowerCase();// removing while spaces
+    return (str == str.split('').reverse().join(''));// 
 }
 
 console.log(isPalindrome("level"));                   // logs 'true'
 console.log(isPalindrome("levels"));                  // logs 'false'
 console.log(isPalindrome("A car, a man, a maraca"));  // logs 'true'F
+str === str.reverse()// single word..
